0719b63ba4fb863338788acb48da96196de4a5b4
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-06 12:01:19 +0100

    Medical App

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {StreamChat} from 'stream-chat'
 import {Chat} from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 
+import {ChannelContainer, ChannelListContainer }
 
 const apiKey = 'gvmwucfu48vm'
+
+// create an instance of a streamchat
+const client = StreamChat.getInstance(apiKey);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChanellListContainer/>
+        <ChanellListContainer/>
+      </Chat>
     </div>
   );
 }
