12b473c9bef6dbefc51b168ecd707502373f6e47
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-07 22:31:48 +0100

    Medical App

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,56 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {StreamChat} from 'stream-chat'
 import {Chat} from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 
-import {ChannelContainer, ChannelListContainer } from './components'
+import {ChannelContainer, ChannelListContainer, Auth } from './components'
 import './App.css'
 
+const cookies = new Cookies();
 const apiKey = 'gvmwucfu48vm'
+const authToken = cookies.get("token");
 
 // create an instance of a streamchat
 const client = StreamChat.getInstance(apiKey);
+
+if(authToken) {
+  client.connectUser({
+      id: cookies.get('userId'),
+      name: cookies.get('username'),
+      fullName: cookies.get('fullName'),
+      image: cookies.get('avatarURL'),
+      hashedPassword: cookies.get('hashedPassword'),
+      phoneNumber: cookies.get('phoneNumber'),
+  }, authToken)
+}
+
 function App() {
+
+  const [createType, setCreateType] = useState('');
+    const [isCreating, setIsCreating] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+
+    if(!authToken) return <Auth />
+
   return (
-    <div className="app__wrapper">
-      <Chat client={client} theme="team light">
-        <ChannelListContainer />
-        <ChannelContainer />
-      </Chat>
+    <div>
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer 
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    setCreateType={setCreateType}
+                    setIsEditing={setIsEditing}
+                />
+                <ChannelContainer 
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    isEditing={isEditing}
+                    setIsEditing={setIsEditing}
+                    createType={createType}
+                />
+            </Chat>
+        </div>
     </div>
   );
 }
