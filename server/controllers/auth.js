e5a3dbef8cb3b472904ad67d499c5e6e07d315d0
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-08 09:53:21 +0100

    Medical Pager

---
+++ server/controllers/auth.js
@@ -0,0 +1,11 @@
+const { connect } = require('getstream');
+const bcrypt = require('bcrypt');
+const StreamChat = require('stream-chat').StreamChat;
+const crypto = require('crypto');
+
+require('dotenv').config();
+
+const signup=()=>{}
+const login=()=>{}
+
+module.exports = { signup, login }
