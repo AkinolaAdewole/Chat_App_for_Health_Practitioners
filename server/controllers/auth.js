7495c7f4bae078742b9e9f4101e4412d5d0e6f67
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-08 10:54:47 +0100

    Medical Pager

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,9 +5,23 @@ const crypto = require('crypto');
 
 require('dotenv').config();
 
-const signup=(req,res)=>{
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const app_id = process.env.STREAM_APP_ID;
+
+const signup= async(req,res)=>{
     try {
-        
+        const { firstname, lastname, username, password, phoneNumber } = req.body;
+
+        const userId = crypto.randomBytes(16).toString('hex');
+
+        const serverClient = connect(api_key, api_secret, app_id);
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const token = serverClient.createUserToken(userId);
+
+        res.status(200).json({ token, firstname, lastname, username, userId, hashedPassword, phoneNumber });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: error });
