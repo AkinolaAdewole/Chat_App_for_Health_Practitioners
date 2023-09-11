0eb337c6abfdb83ee0dc770e8dec16525f846b45
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-11 12:56:27 +0100

    Medical pager

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { useChatContext } from 'stream-chat-react';
+import { useChatContext } from 'stream-chat-react'; // Importing necessary components and hooks
 
-import { UserList } from './';
-import { CloseCreateChannel } from '../assets';
+import { UserList } from './'; // Importing the UserList component
+import { CloseCreateChannel } from '../assets'; // Importing an asset component
 
 const ChannelNameInput = ({ channelName = '', setChannelName }) => {
     const handleChange = (event) => {
@@ -21,20 +21,23 @@ const ChannelNameInput = ({ channelName = '', setChannelName }) => {
 }
 
 const CreateChannel = ({ createType, setIsCreating }) => {
-    const { client, setActiveChannel } = useChatContext();
-    const [selectedUsers, setSelectedUsers] = useState([client.userID || ''])
-    const [channelName, setChannelName] = useState('');
+    const { client, setActiveChannel } = useChatContext(); // Get the chat client and active channel from the context
+    const [selectedUsers, setSelectedUsers] = useState([client.userID || '']) // Initialize selectedUsers with the current user's ID or an empty array
+    const [channelName, setChannelName] = useState(''); // Initialize channelName state
 
     const createChannel = async (e) => {
         e.preventDefault();
 
         try {
+            // Create a new chat channel of the specified type and with provided details
             const newChannel = await client.channel(createType, channelName, {
-                name: channelName, members: selectedUsers
+                name: channelName,
+                members: selectedUsers
             });
 
-            await newChannel.watch();
+            await newChannel.watch(); // Start watching the new channel
 
+            // Reset states and set the newly created channel as the active channel
             setChannelName('');
             setIsCreating(false);
             setSelectedUsers([client.userID]);
@@ -48,12 +51,12 @@ const CreateChannel = ({ createType, setIsCreating }) => {
         <div className="create-channel__container">
             <div className="create-channel__header">
                 <p>{createType === 'team' ? 'Create a New Channel' : 'Send a Direct Message'}</p>
-                <CloseCreateChannel setIsCreating={setIsCreating} />
+                <CloseCreateChannel setIsCreating={setIsCreating} /> {/* Close button */}
             </div>
-            {createType === 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName}/>}
-            <UserList setSelectedUsers={setSelectedUsers} />
+            {createType === 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName} />}
+            <UserList setSelectedUsers={setSelectedUsers} /> {/* UserList component for selecting members */}
             <div className="create-channel__button-wrapper" onClick={createChannel}>
-                <p>{createType === 'team' ? 'Create Channel' : 'Create Message Group'}</p>
+                <p>{createType === 'team' ? 'Create Channel' : 'Create Message Group'}</p> {/* Button to create the channel */}
             </div>
         </div>
     )
