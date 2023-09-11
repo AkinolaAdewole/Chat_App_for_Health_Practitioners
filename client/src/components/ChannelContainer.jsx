09e128fbda56567c6e7fcd7becc49a4536fe1402
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-11 12:17:02 +0100

    Medical pager

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -4,36 +4,40 @@ import { Channel, MessageTeam } from 'stream-chat-react';
 import { ChannelInner, CreateChannel, EditChannel } from './';
 
 const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
-    if(isCreating) {
+    // If the channel is being created, render the CreateChannel component
+    if (isCreating) {
         return (
             <div className="channel__container">
                 <CreateChannel createType={createType} setIsCreating={setIsCreating} />
             </div>
-        )
+        );
     }
 
-    if(isEditing) {
+    // If the channel is being edited, render the EditChannel component
+    if (isEditing) {
         return (
             <div className="channel__container">
                 <EditChannel setIsEditing={setIsEditing} />
-            </div> 
-        )
+            </div>
+        );
     }
 
+    // Define an EmptyState component to display when the chat history is empty
     const EmptyState = () => (
         <div className="channel-empty__container">
             <p className="channel-empty__first">This is the beginning of your chat history.</p>
             <p className="channel-empty__second">Send messages, attachments, links, emojis, and more!</p>
         </div>
-    )
+    );
 
+    // If neither creating nor editing, render the Channel component with its inner content
     return (
         <div className=" channel__container">
             <Channel
-                EmptyStateIndicator={EmptyState}
-                Message={(messageProps, i) => <MessageTeam key={i} {...messageProps} />}
+                EmptyStateIndicator={EmptyState} // Set the EmptyState component as the indicator for an empty channel
+                Message={(messageProps, i) => <MessageTeam key={i} {...messageProps} />} // Render MessageTeam component for messages
             >
-                <ChannelInner setIsEditing={setIsEditing} />
+                <ChannelInner setIsEditing={setIsEditing} /> // Render ChannelInner component
             </Channel>
         </div>
     );
--- client/src/components/EditChannel.jsx
+++ client/src/components/EditChannel.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { useChatContext } from 'stream-chat-react';
 
-import { UserList } from './';
-import { CloseCreateChannel } from '../assets';
+import { UserList } from './'; // Importing the UserList component
+import { CloseCreateChannel } from '../assets'; // Importing an asset component
 
 const ChannelNameInput = ({ channelName = '', setChannelName }) => {
     const handleChange = (event) => {
@@ -21,23 +21,27 @@ const ChannelNameInput = ({ channelName = '', setChannelName }) => {
 }
 
 const EditChannel = ({ setIsEditing }) => {
-    const { channel } = useChatContext();
-    const [channelName, setChannelName] = useState(channel?.data?.name);
-    const [selectedUsers, setSelectedUsers] = useState([])
+    const { channel } = useChatContext(); // Get the current chat channel from the context
+    const [channelName, setChannelName] = useState(channel?.data?.name); // Initialize channelName state with the current channel's name
+    const [selectedUsers, setSelectedUsers] = useState([]); // Initialize selectedUsers state for adding members
 
     const updateChannel = async (event) => {
         event.preventDefault();
 
+        // Check if the channel name has changed
         const nameChanged = channelName !== (channel.data.name || channel.data.id);
 
-        if(nameChanged) {
-            await channel.update({ name: channelName }, { text: `Channel name changed to ${channelName}`});
+        if (nameChanged) {
+            // Update the channel's name and send a text message indicating the change
+            await channel.update({ name: channelName }, { text: `Channel name changed to ${channelName}` });
         }
 
-        if(selectedUsers.length) {
+        if (selectedUsers.length) {
+            // Add selected users as members to the channel
             await channel.addMembers(selectedUsers);
         }
 
+        // Clear the channelName and selectedUsers states, and exit edit mode
         setChannelName(null);
         setIsEditing(false);
         setSelectedUsers([]);
@@ -47,12 +51,12 @@ const EditChannel = ({ setIsEditing }) => {
         <div className="edit-channel__container">
             <div className="edit-channel__header">
                 <p>Edit Channel</p>
-                <CloseCreateChannel setIsEditing={setIsEditing} />
+                <CloseCreateChannel setIsEditing={setIsEditing} /> {/* Close button */}
             </div>
             <ChannelNameInput channelName={channelName} setChannelName={setChannelName} />
-            <UserList setSelectedUsers={setSelectedUsers} />
+            <UserList setSelectedUsers={setSelectedUsers} /> {/* UserList component for selecting members */}
             <div className="edit-channel__button-wrapper" onClick={updateChannel}>
-                <p>Save Changes</p>
+                <p>Save Changes</p> {/* Save Changes button */}
             </div>
         </div>
     )
